0c3b67a5cf3a1fff542cf55b571a640e4da6ecca
Gabriel Morales <gabrielmorales201564@hotmail.com>
2020-09-25 04:45:50 -0400

    coords mouseMove  with useEffect

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,16 +1,27 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Message = () => {
-    
+        
+    const [coords, setCoords] = useState({x:0,y:0});
+    const {x ,y } =coords;
     useEffect(() => {
-        console.log('component mount')
+        // console.log('component mount')
+
+        const mouseMove = (e)=>{
+             const  coords = {x:e.x,y:e.y};
+             setCoords( coords )
+        }
+        window.addEventListener('mousemove',mouseMove);
         return () => {
-            console.log('component unmount')
+            window.removeEventListener('mousemove',mouseMove);
         }
     }, [])
     return (
         <div>
             <h3>Eres Genial</h3>
+            <p>
+                    x:{x} y:{y}
+            </p>
         </div>
     )
 }
